c3985b13737fd4499155abfc63844ae01a0d45e8
ol-iver <scarface@mywoh.de>
2022-11-01 17:45:41 +0100

    Swap tilt open and closed values

--- warema-bridge/rootfs/srv/bridge.js
+++ warema-bridge/rootfs/srv/bridge.js
@@ -72,10 +72,10 @@ function registerDevice(element) {
         tilt_status_topic: 'warema/' + snr + '/tilt',
         set_position_topic: 'warema/' + snr + '/set_position',
         tilt_command_topic: 'warema/' + snr + '/set_tilt',
-        tilt_closed_value: -100,
-        tilt_opened_value: 100,
-        tilt_min: -100,
-        tilt_max: 100,
+        tilt_closed_value: 100,
+        tilt_opened_value: -100,
+        tilt_min: 100,
+        tilt_max: -100,
       }
       break
     case 21:
@@ -93,10 +93,10 @@ function registerDevice(element) {
         tilt_status_topic: 'warema/' + snr + '/tilt',
         set_position_topic: 'warema/' + snr + '/set_position',
         tilt_command_topic: 'warema/' + snr + '/set_tilt',
-        tilt_closed_value: -100,
-        tilt_opened_value: 100,
-        tilt_min: -100,
-        tilt_max: 100,
+        tilt_closed_value: 100,
+        tilt_opened_value: -100,
+        tilt_min: 100,
+        tilt_max: -100,
       }
       break
     case 25:
